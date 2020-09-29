0c22fd4c6aa939e5e89480109dd50d247e30f522
cesar-2000 <67622368+cesar-2000@users.noreply.github.com>
2020-09-29 06:26:13 -0700

    updated cache error

--- app.js
+++ app.js
@@ -5,16 +5,10 @@ var multer = require('multer');
 var fs = require('fs');
 var cors = require('cors');
 const csv = require('csvtojson');
-// const bwipjs = require('bwip-js');
 var zip = require('express-zip');
-const path = require('path');
-
 var JsBarcode = require('jsbarcode');
 var Canvas = require("canvas");
 var canvas = new Canvas.Canvas();
-
-var barcodeJson = []; // from csv file
-var barcodeFiles = []; // generate barcode as png file
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -42,12 +36,14 @@ var upload = multer({
     }
 }).single('file');
 
+var barcodeJson = []; // from csv file
+var barcodeFiles = []; // generate barcode as png file
+
 /** API path that will upload the files */
 app.post('/upload', function (req, res) {
     console.log('csv file upload requesting...')
     barcodeJson = [];
     var tempBarcodeJson = [];
-
 
     upload(req, res, function (err) {
         if (err) {
@@ -103,28 +99,44 @@ app.post('/upload', function (req, res) {
 
 });
 
-app.get('/generate', function (req, res) {
-    console.log('generating barcode is started...')
+var deleteFiles = async () => {
+    return new Promise((resolve, reject) => {
+        if (barcodeFiles.length) {
+            console.log('Deleting barcode images started...');
+            barcodeFiles.forEach((element, i) => {
+                fs.unlink(element.path, function (err) {
+                    if (err) {
+                        console.log('no such file or directory...', err);
+                        reject(err)
+                    }
+                    console.log(`${i} -- File deleted..`);
+                })
+            });
+            console.log('Deleting barcode images finished...');
 
+            barcodeFiles = [];
+            resolve();
+        } else {
+            console.log('barcodeFiles length == 0')
+            resolve();
+        }
+    })
+
+}
+
+app.get('/generate', async function (req, res) {
     if (!barcodeJson.length) {
         res.json({ error_code: 1, err_desc: "Please upload csv file" });
         return;
     }
 
-    if (barcodeFiles.length) {
-        console.log('Deleting barcode images started...');
-        barcodeFiles.forEach(element => {
-            fs.unlink(element.path, function (err) {
-                if (err) return console.log('no such file or directory...', err);
-                console.log('File deleted..');
-            })
-            // fs.unlinkSync(element.path); // Deleting the barcode png
-        });
-        console.log('Deleting barcode images finished...');
+    console.log('barcodeFiles length before>>>', barcodeFiles.length)
 
-        barcodeFiles = [];
-    }
+    await deleteFiles();
+    
+    console.log('barcodeFiles length after>>>', barcodeFiles.length)
 
+    console.log('generating barcode is started...')
 
     barcodeJson.forEach((element, index) => {
         console.log(index, ' element>>>', element)
@@ -169,10 +181,37 @@ app.get('/download', function (req, res) {
     console.log('download api is finished');
 });
 
-app.get('/test', function (req, res) {
-    console.log('test api is working');
-    res.json({ error_code: 0, err_desc: null, message: "test api is working fine" });
-})
+app.get('/key/:keyid/type/:typeid/value/:valueid', function (req, res) {
+    if (req.params.keyid == 'deuyfjdhfd3') {
+        var typeid = req.params.typeid;
+        var valueid = req.params.valueid;
+        
+        if (typeid == 'ean13' || typeid == 'code128') {
+        
+            let type = typeid;
+            let data = valueid;
+            
+            if (type === 'ean13') {
+               data = data.substr(1);
+            }
+            
+            // Using JSBarcode npm package
+            JsBarcode(canvas, data, {
+                format: type,
+                displayValue: true
+            });
+            
+            const buffer = canvas.toBuffer('image/png');
+            
+            var filePath = `./barcodes/${data}.png`;
+            fs.writeFileSync(filePath, buffer);
+            
+            console.log('generating barcode is finished...');
+            res.download(filePath);
+            console.log('barcode image downloaded...');
+        }
+    }
+});
 
 app.use(express.static(__dirname + '/dist/client'));
 
